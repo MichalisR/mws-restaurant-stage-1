0dca1b273e2093dc24621228b40a4a75d54d8f24
Michalis Rodios <michalis.rodios@gmail.com>
2018-07-22 13:47:01 +0300

    Make images responsive

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [
+            {
+              width: 300,
+              quality: 60
+            },
+            {
+              width: 400,
+              quality: 60
+            },
+            {
+              width: 600,
+              quality: 60,
+              suffix: '_2x'
+            },
+            {
+              width: 800,
+              quality: 60,
+              suffix: '_2x'
+            }
+          ]
+        },
+        files: [{
+          expand: true,
+          cwd: 'img_src/',
+          src: ['*.{gif,jpg,png}'],
+          dest: 'img/'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+
+  grunt.registerTask('default', ['responsive_images']);
+}
